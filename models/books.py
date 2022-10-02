ec6c2a9734928a98ebf8d6ad926aa41f8c659525
Takako Onozuka <onozukatakako@gmail.com>
2022-10-02 13:48:38 +0100

    sixth commit

--- models/books.py
+++ models/books.py
@@ -1,11 +1,11 @@
 from models.book import *
 import datetime
 
-book1 = Book("Norwegian wood", "Haruki Murakami", "novel", True, datetime.date(2022, 10, 10))
-book2 = Book("Cooking with Nigela", "Nigela Lawson", "cookery", False, datetime.date(2022, 10, 1))
-book3 = Book("Lord of the flies", "William Golding", "young adult", False, datetime.date(2022, 10, 10))
+book1 = Book("Norwegian wood", "Haruki Murakami", "Novel", True, datetime.date(2022, 10, 10))
+book2 = Book("Cooking with Nigela", "Nigela Lawson", "Cookery", False, datetime.date(2022, 10, 1))
+book3 = Book("Lord of the flies", "William Golding", "Young adult", False, datetime.date(2022, 10, 10))
 book4 = Book("The Minpins", "Roald Dahl", "Children", True,  datetime.date(2022, 10, 20))
-book5 = Book("Ready player one", "Ernest Cline", "young adult",  False, datetime.date(2022, 10, 10))
+book5 = Book("Ready player one", "Ernest Cline", "Young adult",  False, datetime.date(2022, 10, 10))
 # book1 = Book("Norwegian wood", "Haruki Murakami", "novel", True)
 # book2 = Book("Cooking with Nigela", "Nigela Lawson", "cookery", False)
 # book3 = Book("Lord of the flies", "William Golding", "young adult", False)
